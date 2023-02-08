4ac0ddaaeffd11b52a67b31589834831448ac3fd
Petro Mozil <mozil.petryk@gmail.com>
2023-02-08 09:19:43 +0200

    Add seim-random board generation

---
+++ board_generation.py
@@ -0,0 +1,73 @@
+"""The board generation module"""
+import time
+from puzzle import validate_board
+
+
+def linear_generator(
+    seed: int = 14,
+    increment: int = 1012,
+    multiply: int = 251,
+    modulus: int = 8192,
+    length: int = 8
+) -> list[int]:
+    """
+    A simple linear RNG algorithm
+
+    Args:
+        seed: int - the seed
+        increment: int
+        multiply: int
+        modulus: int
+        length: int
+
+    Returns:
+        list[int] - a semi-random list of ints
+    """
+    result: list[int] = []
+    random_val = seed
+    for _ in range(length):
+        random_val = (multiply * random_val + increment) % modulus
+        result.append(random_val)
+
+    return result
+
+
+def generate_line(seed: int, length: int = 9) -> str:
+    """
+    Generate a line for the board
+
+    Args:
+        seed: int - the seed for the line
+        length: int - the length of the line
+
+    Returns:
+        str - the line
+    """
+    line = linear_generator(seed=seed, modulus=1024, length=length)
+    result = ""
+    for i in line:
+        if i > 900:
+            result += str(i % 10)
+        elif i > 600:
+            result += " "
+        else:
+            result += "*"
+    return result
+
+
+def generate_board(lines: int = 9) -> list[str]:
+    """
+    Generate the board
+
+    Args:
+        lines: int - the board size
+
+    Returns:
+        list[str] - a list of strings (the board)
+    """
+    return [
+        generate_line(
+            seed=int(time.time()*100000),
+            length=lines
+        ) for _ in range(lines)
+    ]
